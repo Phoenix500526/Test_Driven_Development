8ee7854d5e4916906bcf816fd4e6b1788a4341f2
Jiawei Zhao <Phoenxi500526@163.com>
2022-08-05 17:38:38 +0800

    add pattern check and pass it

--- ArgsParser/ArgsParser.h
+++ ArgsParser/ArgsParser.h
@@ -1,7 +1,9 @@
 #ifndef TDD_ARGPARSER_H
 #define TDD_ARGPARSER_H
 #include <string_view>
-
+#include <stdexcept>
+#include <algorithm>
+#include <iostream>
 class ArgsParser
 {
 private:
@@ -10,7 +12,24 @@ private:
     
 public:
     ArgsParser(const char* pattern, const char* args)
-        : pattern_(pattern), args_(args){}
+        : pattern_(pattern), args_(args){
+            size_t cnt_of_comma = std::count(pattern_.begin(), pattern_.end(), ',');
+            size_t cnt_of_colon = std::count(pattern_.begin(), pattern_.end(), ':');
+            if(cnt_of_comma != cnt_of_colon - 1){
+                throw std::invalid_argument("Invalid Pattern: ");
+            }
+            size_t start = 0, end = pattern_.find(',');
+            while(end != std::string_view::npos){
+                size_t len = end - start;
+                std::string_view temp = pattern_.substr(start, len);
+                size_t colon_pos = temp.find(':');
+                if(colon_pos == std::string_view::npos || colon_pos == 0 || colon_pos == temp.size() - 1)
+                    throw std::invalid_argument("Invalid Pattern: ");
+                start = end + 1;
+                end = pattern_.find(',', start);
+            }
+
+        }
     ~ArgsParser(){}
 
     constexpr size_t get_idx(const char c){
--- ArgsParser/ArgsTest.cc
+++ ArgsParser/ArgsTest.cc
@@ -1,6 +1,10 @@
 #include <gtest/gtest.h>
+#include <stdexcept>
 #include "ArgsParser.h"
 // 添加接口设计方式
+using namespace std;
+
+
 TEST(ArgsTest, GetIndexFromPattern) {
      // Example: -l -p 8080 -d /var/log
      // Happy Path
@@ -9,4 +13,8 @@ TEST(ArgsTest, GetIndexFromPattern) {
      EXPECT_EQ(args.get_idx('p'), 1);
      EXPECT_EQ(args.get_idx('d'), 2);
      EXPECT_EQ(args.get_idx('f'), -1);
+     // Sad Path
+     ASSERT_THROW(ArgsParser("l:bp:i,d:s", "-l -p 8080 -d /varlog"), invalid_argument);
+     ASSERT_THROW(ArgsParser(":lb,p:i,d:s", "-l -p 8080 -d /varlog"), invalid_argument) << __LINE__ << '\n';
+     ASSERT_THROW(ArgsParser(":lb,p:i,d:s", "-l -p 8080 -d /varlog"), invalid_argument);
 }
