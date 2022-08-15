3c63ee5c113c3c9b044e7f9c7692096ab713831a
Jiawei Zhao <Phoenxi500526@163.com>
2022-08-15 17:05:17 +0800

    back to the square one

--- ArgsParser/ArgsParser.h
+++ ArgsParser/ArgsParser.h
@@ -1,40 +1,19 @@
 #ifndef TDD_ARGPARSER_H
 #define TDD_ARGPARSER_H
 #include <string_view>
-#include <stdexcept>
-#include <algorithm>
-#include <cctype>
-#include <any>
-#include <unordered_map>
-#include "TypeList.h"
 
 
-template <typename... Args>
 class ArgsParser
 {
 private:
-    using TypeList_t = TypeList<Args...>;
     std::string_view pattern_;
     std::string_view args_;
-    std::unordered_map<char, std::any> table_;
     
-    void pattern_check(){
-        bool charset[26] = {false};
-        for(const char c : pattern_){
-            if(!std::islower(c) || charset[c - 'a'])
-                throw std::invalid_argument("Invalid Pattern");
-            charset[c - 'a'] = true;
-        }
-    }
-
-    void arguments_check(){
-
-    }
+    
 public:
     ArgsParser(const char* pattern, const char* args)
         : pattern_(pattern), args_(args){
-            pattern_check();
-            arguments_check();
+            
         }
     ~ArgsParser(){}
 
--- ArgsParser/ArgsTest.cc
+++ ArgsParser/ArgsTest.cc
@@ -1,47 +1,27 @@
 #include <gtest/gtest.h>
-#include <stdexcept>
-#include <functional>
 #include "ArgsParser.h"
-#include "TypeList.h"
 // 添加接口设计方式
 using namespace std;
 
-TEST(ArgsTest, TypeListTest){
-     using typelist_t = TypeList<int, double, char, string_view>;
-     ASSERT_EQ(Length_v<typelist_t>, 4);
-     static_assert(is_same_v<TypeAt_t<typelist_t, 0>, int>, "typelist_t[0] is not int type");
-     static_assert(is_same_v<TypeAt_t<typelist_t, 1>, double>, "typelist_t[1] is not double type");
-     static_assert(is_same_v<TypeAt_t<typelist_t, 2>, char>, "typelist_t[2] is not char type");
-     static_assert(is_same_v<TypeAt_t<typelist_t, 3>, string_view>, "typelist_t[3] is not string_views type");
+// Example:
+//  Pattern: l:b,p:n,d:s  Args: -l -p 8080 -d /var/log
+
+TEST(ArgsTest, should_construct_ArgsParser_Object){
+     // Happy Path:
+     // - TODO: construct a ArgsParser Object 
+     ASSERT_NO_THROW(ArgsParser("l:b,p:n,d:s", "-l -p 8080 -d /var/log"));
 }
 
-TEST(ArgsTest, ArgsParserConstructor) {
-     // Example: -l -p 8080 -d /var/log
-     auto construtor_wrapper = [](const char* pattern, const char* args){
-          ArgsParser<bool,int,string_view>(pattern, args);
-     };
+TEST(ArgsTest, should_throw_InvalidPatternError_when_pass_a_invalid_pattern){
+     GTEST_SKIP();
+     // Sad Path: Invalid Pattern
+     // TODO: l:bp:n,d:s
+     // TODO: lb:,p:n,d:s
+     // TODO: 1:b,p:n,d:s, l!b,p:n,d:s
 
-     // Happy Path
-     auto pattern_checker = bind(construtor_wrapper, placeholders::_1, "-l -p 8080 -d /var/log");
-     ASSERT_NO_THROW(pattern_checker("lpd"));
-     
-     // Sad Path
-     // TODO: 
-     // 1. lpD
-     // 2. lp1d
-     // 3. lpld
-     ASSERT_THROW(pattern_checker("lpD"), invalid_argument);
-     ASSERT_THROW(pattern_checker("lp1d"), invalid_argument);
-     ASSERT_THROW(pattern_checker("lpld"), invalid_argument);
-
-     // Sad Path
-     // TODO:
-     // 1. "-l 123"
-     // 2. "-l -p -d /var/log"
-     // 3. "-l -p 8080 8081 -d /var/log"
-     auto arguments_checker = bind(construtor_wrapper, "lpd", placeholders::_1);
-     ASSERT_THROW(arguments_checker("-l 123"), invalid_argument);
-     
-     // ASSERT_THROW(ArgsParser("l:b,p:i,d:s", "-l -p 8080 8081 -d /varlog"), invalid_argument);
-     // ASSERT_THROW(ArgsParser("l:b,p:i,d:s", "-l -p -d /varlog"), invalid_argument);
+     // Sad Path: Invalid Args
+     // TODO:  "-l 123"
+     // TODO: "-l -p -d /var/log"
+     // TODO: "-l -p 8080 8081 -d /var/log"
 }
+
