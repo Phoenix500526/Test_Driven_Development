61d76296adcae874dc4be27ea2746f89d2fdfea4
Jiawei Zhao <Phoenxi500526@163.com>
2022-08-16 15:10:26 +0800

    refactor: removing some condition statements

--- ArgsParser/ArgsParser.h
+++ ArgsParser/ArgsParser.h
@@ -2,6 +2,7 @@
 #define TDD_ARGPARSER_H
 #include <string_view>
 #include <cctype>
+#include <functional>
 #include "ArgsExceptions.h"
 
 class ArgsParser
@@ -10,23 +11,32 @@ private:
     std::string_view pattern_;
     std::string_view args_;
     
+    
     void pattern_check(){
+        constexpr bool supported_pattern[26] = {
+            0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 
+            1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0
+        };
+        std::function<void(const size_t)> predicator[4];
+        predicator[0] = [this](const size_t idx){
+            if(!std::islower(this->pattern_[idx]))
+                throw InvalidPattern("Invalid Pattern: Invalid Option");
+        };
+        predicator[1] = [this](const size_t idx){
+            if(this->pattern_[idx] != ':')
+                throw InvalidPattern("Invalid Pattern: Colons Error");
+        };
+        predicator[2] = [this, &supported_pattern](const size_t idx){
+            if(!std::islower(this->pattern_[idx]) || !supported_pattern[this->pattern_[idx] - 'a'])
+                throw InvalidPattern("Invalid Pattern: Unsupproted Option Type");
+        };
+        predicator[3] = [this](const size_t idx){
+            if(this->pattern_[idx] != ',')
+                throw InvalidPattern("Invalid Pattern: Commas Error");
+        };
         size_t mask = 0b11;
         for(int idx = 0; idx < pattern_.length(); ++idx){
-            if((idx & mask) == 1){
-                if (pattern_[idx] != ':')
-                {
-                    throw InvalidPattern("Invalid Pattern: Colons error(s)");
-                }
-            }else if((idx & mask) == 3){
-                if (pattern_[idx] != ',')
-                {
-                    throw InvalidPattern("Invalid Pattern: Comma error(s)");
-                }
-            }else{
-                if(!std::islower(pattern_[idx]))
-                    throw InvalidPattern("Invalid Pattern: Unsupported symbol");
-            }
+            predicator[idx & mask](idx);     
         }
     }
     
