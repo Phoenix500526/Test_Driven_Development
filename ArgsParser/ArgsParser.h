bcd6e3b5762a1dd2d73107d1625366e420c71dd5
Jiawei Zhao <Phoenxi500526@163.com>
2022-08-17 21:27:32 +0800

    REFACTOR: eliminate some bad smell —— Duplicated Code

--- ArgsParser/ArgsParser.h
+++ ArgsParser/ArgsParser.h
@@ -6,6 +6,7 @@
 #include <unordered_map>
 #include <charconv>
 #include "ArgsExceptions.h"
+#include <tuple>
 #include <iostream>
 class ArgsParser
 {
@@ -13,92 +14,95 @@ private:
     std::string_view pattern_;
     std::string_view args_;
     std::unordered_map<char, std::any> table_;
+    inline static char whitespaces_[26] = {
+             0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+             2, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0
+         };
+    std::function<void(const char, const std::string_view)> parameter_generator_[26];
+    std::function<void(const size_t)> predicator_[4];
     
-    void pattern_check(){
-        constexpr bool supported_pattern[26] = {
-            0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
-            1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0
-        };
-        std::function<void(const size_t)> predicator[4];
-        predicator[0] = [this](const size_t idx){
-            if(!std::islower(this->pattern_[idx]))
+    inline void init_pattern_checker(){
+        predicator_[0] = [this](const size_t idx){
+            if(!std::islower(pattern_[idx]))
                 throw InvalidPattern("Invalid Pattern: Invalid Option");
         };
-        predicator[1] = [this](const size_t idx){
-            if(this->pattern_[idx] != ':')
+        predicator_[1] = [this](const size_t idx){
+            if(pattern_[idx] != ':')
                 throw InvalidPattern("Invalid Pattern: Colons Error");
         };
-        predicator[2] = [this, &supported_pattern](const size_t idx){
-            if(!std::islower(this->pattern_[idx]) || !supported_pattern[this->pattern_[idx] - 'a'])
+        predicator_[2] = [this](const size_t idx){
+            if(!std::islower(pattern_[idx]) || !whitespaces_[pattern_[idx] - 'a'])
                 throw InvalidPattern("Invalid Pattern: Unsupproted Option Type");
         };
-        predicator[3] = [this](const size_t idx){
-            if(this->pattern_[idx] != ',')
+        predicator_[3] = [this](const size_t idx){
+            if(pattern_[idx] != ',')
                 throw InvalidPattern("Invalid Pattern: Commas Error");
         };
+    }
 
+    inline void init_parameter_generator(){
+        parameter_generator_['b' - 'a'] = [this](const char opt, const std::string_view){
+            this->table_[opt] = std::make_any<bool>(true);
+        };
+
+        parameter_generator_['n' - 'a'] = [this](const char opt, const std::string_view value_str){
+            int32_t value;
+            if(std::from_chars(value_str.begin(), value_str.end(), value).ec != std::errc{}){
+                throw InvalidArgument("Invalid Argument: Unmatch type and argument");
+            }
+            this->table_[opt] =  std::make_any<int32_t>(value);
+        };
+
+        parameter_generator_['s' - 'a'] = [this](const char opt, const std::string_view value_str){
+            this->table_[opt] =  std::make_any<std::string_view>(value_str);
+        };
+    }
+
+    void init(){
+        init_pattern_checker();
+        init_parameter_generator();
+    }
+
+    void pattern_check(){
         size_t mask = 0b11;
         for(int idx = 0; idx < pattern_.length(); ++idx){
-            predicator[idx & mask](idx);     
+            predicator_[idx & mask](idx);     
         }
     }
 
+    inline std::string_view get_option_and_parameter(std::string_view str){
+        auto parameter = str.substr(2);
+        auto last_whitespace = parameter.find_last_of(' ');
+        if (last_whitespace != std::string_view::npos) {
+            parameter.remove_suffix(parameter.size() - last_whitespace);
+        }
+        return parameter;
+    };
+
+
+    void check_whitespace(const size_t start, const size_t end){
+        size_t idx = pattern_.find(args_[start]);
+        if(idx == std::string_view::npos)
+            throw InvalidArgument("Invalid Argument: Undefined  Argument");
+        size_t type_idx = idx + 2;
+        size_t length = end != std::string_view::npos ? end - start : end;
+        size_t whitespace_cnt = end != std::string_view::npos ? 
+                                whitespaces_[pattern_[type_idx] - 'a'] :
+                                whitespaces_[pattern_[type_idx] - 'a'] - 1;
+    
+        std::string_view sub_str = args_.substr(start, length);
+
+        auto cnt = std::count(sub_str.begin(), sub_str.end(), ' ');
+        if(cnt != whitespace_cnt)
+                throw InvalidArgument("Invalid Argument: Wrong Number of Parameters");
+
+        auto value_str = get_option_and_parameter(sub_str);
+        parameter_generator_[pattern_[type_idx] - 'a'](args_[start], value_str);
+    }
+
+
     void arguments_check(){
-        const int whitespaces[26] = {
-            0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
-            2, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0
-        };
         size_t idx = 0;
-
-        auto check_whitespace = [this, &whitespaces](const size_t start, const size_t end){
-            size_t idx = this->pattern_.find(this->args_[start]);
-            if(idx == std::string_view::npos)
-                throw InvalidArgument("Invalid Argument: Undefined  Argument");
-            size_t type_idx = idx + 2;
-            size_t length = end != std::string_view::npos ? end - start : end;
-            size_t whitespace_cnt = end != std::string_view::npos ? 
-                                    whitespaces[this->pattern_[type_idx] - 'a'] :
-                                    whitespaces[this->pattern_[type_idx] - 'a'] - 1;
-        
-            std::string_view sub_str = this->args_.substr(start, length);
-
-            auto cnt = std::count(sub_str.begin(), sub_str.end(), ' ');
-            if(cnt != whitespace_cnt)
-                    throw InvalidArgument("Invalid Argument: Wrong Number of Parameters");
-            switch (this->pattern_[type_idx])
-            {
-            case 'b':
-                this->table_[this->args_[start]] = std::make_any<bool>(true);
-                break;
-            case 'n':{
-                auto value_str = sub_str.substr(2);
-                auto last_whitespace = value_str.find_last_of(' ');
-                if (last_whitespace != std::string_view::npos) {
-                    value_str.remove_suffix(value_str.size() - last_whitespace);
-                }
-                int32_t value;
-                if(std::from_chars(value_str.begin(), value_str.end(), value).ec != std::errc{}){
-                    throw InvalidArgument("Invalid Argument: Unmatch type and argument");
-                }
-                this->table_[this->args_[start]] = std::make_any<int32_t>(value);
-                break;
-            }
-            case 's':{
-                auto value_str = sub_str.substr(2);
-                auto last_whitespace = value_str.find_last_of(' ');
-                if (last_whitespace != std::string_view::npos) {
-                    value_str.remove_suffix(value_str.size() - last_whitespace);
-                }
-                this->table_[this->args_[start]] = std::make_any<std::string_view>(value_str);
-                break;
-            }
-            default:
-                break;
-            }
-            
-
-        };
-
         while(idx != std::string_view::npos){
             size_t next_hyphen = args_.find('-', idx + 1);
             check_whitespace(idx + 1, next_hyphen);
@@ -109,6 +113,7 @@ private:
 public:
     ArgsParser(const char* pattern, const char* args)
         : pattern_(pattern), args_(args){
+            init();
             pattern_check();
             arguments_check();
         }
